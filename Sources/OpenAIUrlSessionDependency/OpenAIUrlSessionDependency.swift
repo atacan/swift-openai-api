e3c4d804392c00894def63ce1a86b8a81c15044a
atacan <atacandur@icloud.com>
2024-02-18 11:46:01 +0100

    Add OpenAI API key as a dependency

--- Sources/OpenAIUrlSessionDependency/OpenAIUrlSessionDependency.swift
+++ Sources/OpenAIUrlSessionDependency/OpenAIUrlSessionDependency.swift
@@ -8,18 +8,19 @@ import SwiftUI
 
 public struct OpenAIUrlSessionDependency {
     //    var fetch: @Sendable (Int) async throws -> String
-    var createChatCompletion: @Sendable (_ input: Operations.createChatCompletion.Input) async throws -> Operations
+    public var createChatCompletion: @Sendable (_ input: Operations.createChatCompletion.Input) async throws -> Operations
         .createChatCompletion.Output
 }
 
 extension OpenAIUrlSessionDependency: DependencyKey {
     public static var liveValue: Self {
+        @Dependency(\.secrets) var secrets
         let client = Client(
             serverURL: URL(string: "https://api.openai.com/v1")!,
             transport: URLSessionTransport(),
-            middlewares: [AuthenticationMiddleware(apiKey: "sk-")]
+            middlewares: [AuthenticationMiddleware(apiKey: secrets.openAIKey())]
         )
-
+        
         return Self(
             createChatCompletion: { try await client.createChatCompletion($0) }
         )
@@ -36,7 +37,7 @@ extension DependencyValues {
 /// Injects an authorization header to every request.
 struct AuthenticationMiddleware: ClientMiddleware {
     let apiKey: String
-
+    
     func intercept(
         _ request: HTTPRequest,
         body: HTTPBody?,
@@ -46,7 +47,26 @@ struct AuthenticationMiddleware: ClientMiddleware {
     ) async throws -> (HTTPResponse, HTTPBody?) {
         var request = request
         request.headerFields.append(.init(name: .authorization, value: "Bearer \(apiKey)"))
-
+        
         return try await next(request, body, baseURL)
+    }
+}
+
+public struct SecretsDependency: DependencyKey {
+    public var openAIKey: @Sendable ()->String
+    
+    public init(openAIKey: @Sendable @escaping () -> String) {
+        self.openAIKey = openAIKey
+    }
+    
+    public static var liveValue: SecretsDependency {
+        Self(openAIKey: {"bring your own key"})
+    }
+}
+
+extension DependencyValues {
+    public var secrets: SecretsDependency {
+        get { self[SecretsDependency.self] }
+        set { self[SecretsDependency.self] = newValue }
     }
 }
