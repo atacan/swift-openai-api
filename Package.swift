52ad4ba530e32217afac45f7eb6bd897db58846f
atacan <atacandur@icloud.com>
2024-03-18 11:00:37 +0100

    remove dependency lib

--- Package.swift
+++ Package.swift
@@ -7,23 +7,19 @@ let package = Package(
     name: "SwiftOpenAIAPI",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "SwiftOpenAIAPI", targets: ["SwiftOpenAIAPI"]),
-
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "OpenAIUrlSessionClient", targets: ["OpenAIUrlSessionClient"]),
-        .library(name: "OpenAIUrlSessionDependency", targets: ["OpenAIUrlSessionDependency"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0"),
-        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
     ],
     targets: [
+        .target(name: "SwiftOpenAIAPI"),
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(name: "SwiftOpenAIAPI"),
-
         .target(
             name: "OpenAIUrlSessionClient",
             dependencies: [
@@ -32,10 +28,6 @@ let package = Package(
             ],
             plugins: [.plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator")]
         ),
-        .target(name: "OpenAIUrlSessionDependency", dependencies: [
-            "OpenAIUrlSessionClient",
-            .product(name: "Dependencies", package: "swift-dependencies"),
-        ]),
         .testTarget(
             name: "SwiftOpenAIAPITests",
             dependencies: ["SwiftOpenAIAPI"]
